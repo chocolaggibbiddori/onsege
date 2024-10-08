6559c6f6d68deba4f3f2d819bce9f981b3e07861
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-10-08 23:02:25 +0900

    마무리

--- counter-server/src/main/java/ch/onsege/counter/CounterServerApplication.java
+++ counter-server/src/main/java/ch/onsege/counter/CounterServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OthersApplication {
+public class CounterServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OthersApplication.class, args);
+		SpringApplication.run(CounterServerApplication.class, args);
 	}
 }
