4c8f9b9efefc2db9fd1c825f5440c6f1c289aa3e
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-10-15 03:39:08 +0900

    헬스 체크 추가

--- counter-server/src/main/java/ch/onsege/counter/controller/HomeController.java
+++ counter-server/src/main/java/ch/onsege/counter/controller/HomeController.java
@@ -2,6 +2,7 @@ package ch.onsege.counter.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,11 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         return "redirect:/counter";
+    }
+
+    @GetMapping("/health")
+    @ResponseBody
+    public String health() {
+        return "{\"status\": \"UP\"}";
     }
 }
